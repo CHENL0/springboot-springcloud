9db9ef9c3181b96a87ab3ea170f6380cd028a44c
CHENL0 <893457378@qq.com>
2018-12-27 00:50:25 +0800

    add zuul

---
+++ spring-cloud-zuul/src/main/java/com/spring/cloud/zuul/ZuulApplication.java
@@ -0,0 +1,20 @@
+package com.spring.cloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Chen
+ * @createTime 1127 0:07
+ * @description 1
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
